0492e5e3e60f55440eac50606039737370694bee
Anmol Chopra <anmol.chopra1212@gmail.com>
2021-07-30 13:36:36 +0530

    Home Page Added

--- app/src/main/java/com/anmol/picex/LoginPage.kt
+++ app/src/main/java/com/anmol/picex/LoginPage.kt
@@ -1,20 +1,56 @@
 package com.anmol.picex
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class LoginPage : AppCompatActivity() {
-    lateinit var button: Button
+    lateinit var loginbt: Button
+    lateinit var passcodetxt: EditText
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var forgotbt: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
-        val temp = intent.getStringExtra("Passcode")
-
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
-            Toast.makeText(this@LoginPage,temp, Toast.LENGTH_SHORT).show()
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+        if(isLoggedIn){
+            val intent = Intent ( this@LoginPage, HomePage::class.java)
+            startActivity(intent)
+            finish()
         }
+        val temp = intent.getStringExtra("Passcode")
+        loginbt = findViewById(R.id.loginbt)
+        passcodetxt = findViewById(R.id.passcodetxt)
+        forgotbt = findViewById(R.id.forgotbt)
+
+        loginbt.setOnClickListener {
+            val passcodesp = sharedPreferences.getString("passwordsp", "1234")
+            val password = passcodetxt.text.toString()
+            if(password == passcodesp){
+                savepreference()
+                startActivity(Intent(this@LoginPage, HomePage::class.java))
+            }
+
+            else{
+                Toast.makeText(this@LoginPage,"Enter the details", Toast.LENGTH_SHORT).show()
+            }
+        }
+        forgotbt.setOnClickListener {
+            forgotpreference()
+            startActivity(Intent(this@LoginPage, MainRegister::class.java))
+        }
+    }
+    fun savepreference(){
+        sharedPreferences.edit().putBoolean("isLoggedIn",true).apply()
+    }
+    fun forgotpreference(){
+        sharedPreferences.edit().putBoolean("isresIn", false).apply()
     }
 }
--- app/src/main/java/com/anmol/picex/MainRegister.kt
+++ app/src/main/java/com/anmol/picex/MainRegister.kt
@@ -1,6 +1,8 @@
 package com.anmol.picex
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,23 +14,54 @@ class MainRegister : AppCompatActivity() {
     lateinit var nametxt: EditText
     lateinit var passcode: EditText
     lateinit var registerbt: Button
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//     func1    Register screen will not show again
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        val isresIn = sharedPreferences.getBoolean("isresIn", false)
+        setContentView(R.layout.activity_login_page)
+        if(isresIn){
+            val intent = Intent ( this@MainRegister, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+//        func1 ends
+
         setContentView(R.layout.register_main)
 
         nametxt = findViewById(R.id.nametxt)
-        passcode = findViewById(R.id.passcode) as EditText
+        passcode = findViewById<EditText>(R.id.passcode)
         registerbt = findViewById(R.id.registerbt)
-        val intent = Intent(this@MainRegister, LoginPage::class.java)
-        val pass = passcode.text
+
 
         registerbt.setOnClickListener {
-//            Toast.makeText(this@MainRegister,passcode.text, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainRegister, LoginPage::class.java)
+            val pass: String? = passcode.text.toString()
+            val nameid: String? = nametxt.text.toString()
+            if(nameid != null && pass != null && nameid!="" && pass != ""){
+                savepreference(pass)
+                intent.putExtra("Passcode", pass)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this@MainRegister,"Enter the details", Toast.LENGTH_SHORT).show()
+            }
+//            Toast.makeText(this@MainRegister,pass, Toast.LENGTH_SHORT).show()
 
-            intent.putExtra("Passcode", pass)
-            startActivity(intent)
 
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+    fun savepreference(passcode: String){
+        sharedPreferences.edit().putBoolean("isresIn",true).apply()
+        sharedPreferences.edit().putString("passwordsp",passcode).apply()
+    }
+
 }
